558befe34b7ff6dd18f1b782a7dff57fefee4f37
gurukota <simbarashedixon@gmail.com>
2021-01-06 12:31:00 +0200

    Minor fix

--- controller/provinceController.js
+++ controller/provinceController.js
@@ -15,19 +15,3 @@ module.exports.postProvince = async (req, res) => {
     }
     return res.status(response.status).send(response)
 }
-
-module.exports.getProvinceSummary = async (req, res) => {
-    let response = {};
-    try {
-        const responsefromService = await provinceService.getProvinceSummary();
-        response.status = 200;
-        response.message = 'Posted successfully';
-        response.body = responsefromService;
-    } catch (error) {
-        console.log('Something went wrong: Controller: postProvince', error);
-        response.status = 400;
-        response.message = error.message;
-        response.body = {};
-    }
-    return res.status(response.status).send(response)
-}

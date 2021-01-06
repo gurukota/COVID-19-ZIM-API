389f4a31d7e6bcec0ad091f89958490fe797782c
gurukota <simbarashedixon@gmail.com>
2021-01-06 15:07:01 +0200

    changes

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,21 @@
-var MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-module.exports = async () => {
-    const url = "mongodb+srv://tinman:tinman@cluster0.3vqhy.mongodb.net/covid?retryWrites=true&w=majority"
-    try {
-        await MongoClient.connect(url,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log('Database Connected Successfully');
-    } catch (error) {
-        console.log('Database Connectivity Error', error);
-        throw new Error(error);
-    }
-};
+// module.exports = async () => {
+//     const url = "mongodb+srv://tinman:tinman@cluster0.3vqhy.mongodb.net/covid?retryWrites=true&w=majority"
+//     try {
+//         await MongoClient.connect(url,{
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         })
+//         console.log('Database Connected Successfully');
+//     } catch (error) {
+//         console.log('Database Connectivity Error', error);
+//         throw new Error(error);
+//     }
+// };
 
+const url = "mongodb+srv://tinman:tinman@cluster0.3vqhy.mongodb.net/covid?retryWrites=true&w=majority"
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));

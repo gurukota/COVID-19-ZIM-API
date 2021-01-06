ee7ad28bb81e7e915b851606c686e73c09f9c8d3
gurukota <simbarashedixon@gmail.com>
2021-01-06 17:05:11 +0200

    Final changes

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.urlencoded({extended: true}));
 app.use(require('./routes/route'));
 app.use('/', (req, res)=>{
     res.json({
-        "/:": "Covid 19 API Zimbabwe",
-        "/summary:": "Covid 19 API Zimbabwe",
-        "/provinces:": "Covid 19 API Zimbabwe",
-        "/province/provincename:": "Covid 19 API Zimbabwe"
+        "/": "Covid 19 API Zimbabwe",
+        "/summary": "Covid 19 API Zimbabwe",
+        "/provinces": "Covid 19 API Zimbabwe",
+        "/province/:provincename": "Covid 19 API Zimbabwe"
     });
 });
 const port = process.env.PORT || 3001

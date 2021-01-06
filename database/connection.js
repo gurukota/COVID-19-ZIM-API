a646874fa8803e10dd565687810405ed5ae4e463
gurukota <simbarashedixon@gmail.com>
2021-01-06 12:35:43 +0200

    mongo url tweak

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = async () => {
     
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI,{
+        await mongoose.connect(process.env.MONGODB_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true
         })

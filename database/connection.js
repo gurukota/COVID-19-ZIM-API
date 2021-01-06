279a79ec79758f4504a976a36ef909509634b9cd
gurukota <simbarashedixon@gmail.com>
2021-01-06 14:59:43 +0200

    changes

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,26 @@
-var MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
+// var MongoClient = require('mongodb').MongoClient;
+// require('dotenv').config();
 
-module.exports = async () => {
+// module.exports = async () => {
     
-    try {
-        await MongoClient.connect(process.env.MONGODB_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log('Database Connected Successfully');
-    } catch (error) {
-        console.log('Database Connectivity Error', error);
-        throw new Error(error);
-    }
-};
+//     try {
+//         await MongoClient.connect(process.env.MONGODB_URI,{
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         })
+//         console.log('Database Connected Successfully');
+//     } catch (error) {
+//         console.log('Database Connectivity Error', error);
+//         throw new Error(error);
+//     }
+// };
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://tinman:tinman@cluster0.3vqhy.mongodb.net/covid?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});

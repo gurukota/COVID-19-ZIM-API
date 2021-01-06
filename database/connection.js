3262fd516c013ed1564de83dc981bd782135e27a
gurukota <simbarashedixon@gmail.com>
2021-01-06 12:50:15 +0200

    Added MongoClient

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongodb').MongoClient;
 
 module.exports = async () => {
     
     try {
-        await mongoose.connect(process.env.MONGODB_URI,{
+        await mongoose.connect(process.env.DB_URL,{
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
